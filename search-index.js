5865e7c920d5e3de1114a2b7fdfbe99de3b2a688
shenanigansd <shenanigansd@users.noreply.github.com>
2022-06-01 03:25:02 +0000

    deploy: b71a5beeaf7d82c33a2d83d6fe72abda04411c94

---
+++ search-index.js
@@ -0,0 +1,5 @@
+var searchIndex = JSON.parse('{\
+"shelby":{"doc":"","t":[],"n":[],"q":[],"d":[],"i":[],"f":[],"p":[]},\
+"shelby_cli":{"doc":"","t":[5],"n":["main"],"q":["shelby_cli"],"d":[""],"i":[0],"f":[[[]]],"p":[]}\
+}');
+if (window.initSearch) {window.initSearch(searchIndex)};
---
+++ source-files.js
@@ -0,0 +1,4 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["shelby"] = {"name":"","files":["lib.rs"]};
+sourcesIndex["shelby_cli"] = {"name":"","files":["main.rs"]};
+createSourceSidebar();
